70aea87b5efa45c31d1daa7259bc0ce0a4a46288
oblivionangel <jaylin.yu@open-mesh.com>
2016-08-06 21:50:44 +0800

    123

--- package/firmware-ev/files/root/setup_vpn.sh
+++ package/firmware-ev/files/root/setup_vpn.sh
@@ -13,12 +13,12 @@
         ipsec up evpowergroup
         sleep 3
         echo "c evpowergroup" > ../var/run/xl2tpd/l2tp-control
-        sleep 4
+        sleep 5
         PPP_INT=`./root/getint.sh`
         echo $PPP_INT
         PPP_GW_ADD=`./root/getip.sh $PPP_INT`
         echo $PPP_GW_ADD
         route add -net 10.9.8.0 gateway $PPP_GW_ADD netmask 255.255.248.0 dev $PPP_INT
+	route add -net 10.9.8.0 gateway 10.9.8.100 netmask 255.255.248.0 dev ppp0
 	route add -net 10.9.8.0 gateway 10.9.8.100 netmask 255.255.248.0 dev ppp1
-	route add -net 10.9.8.0 gateway 10.9.8.100 netmask 255.255.248.0 dev ppp2
 
